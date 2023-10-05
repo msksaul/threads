523875b3c66c3a09816eafaafd467cd2e1d695d9
mscalel <msksaul@gmail.com>
2023-10-05 17:39:59 -0600

    Home Page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,12 +2,16 @@ import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Topbar from '@/components/shared/Topbar'
+import Bottombar from '@/components/shared/Bottombar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Threads',
-  description: 'Threads Clone App',
+  description: 'A Next.js 13 Meta Trheads Application'
 }
 
 export default function RootLayout({
@@ -18,7 +22,23 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+              {children}
+              </div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
