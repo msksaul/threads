a96c9669726a1a9d2eec3aae6e866738aec461d8
mscalel <msksaul@gmail.com>
2023-10-18 17:18:56 -0600

    fetchPosts Homepage

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -35,3 +35,31 @@ export async function createThread({ text, author, communityId, path}: Params) {
 
   
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB()
+
+  const skipAmount = (pageNumber -1) * pageSize
+
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] }})
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image'
+      }
+    })
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] }})
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostsCount > skipAmount + posts.length
+
+    return { posts, isNext }
+}
