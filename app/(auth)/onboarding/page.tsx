2a77aa605f62618cdd200103ca3b6d9acf55da70
mscalel <msksaul@gmail.com>
2023-10-25 17:07:36 -0600

    Community API

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,15 +9,15 @@ async function Page() {
   if(!user) return null
 
   const userInfo = await fetchUser(user.id)
-  if(!userInfo?.onboarded) redirect('/')
+  if(userInfo?.onboarded) redirect('/')
 
   const userData = {
     id: user.id,
-    objectId: userInfo._id.toString(),
-    username: userInfo.username || user.username,
-    name: userInfo.name || user.firstName || '',
-    bio: userInfo.bio || '',
-    image: userInfo.image || user.imageUrl
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? '',
+    bio: userInfo ? userInfo?.bio : '',
+    image: userInfo ? userInfo?.image : user.imageUrl
   }
 
   return (
